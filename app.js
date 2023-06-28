a74cb69393722359660f0d6cd1f598013c78f2a3
PdMendoza <pedromendozaagramonte@gmail.com>
2023-06-27 22:45:37 -0400

    cleaning up and setting heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const https = require("https");
 const request = require("request");
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
-const port = 4040;
+const port = process.env.PORT;
 const app = express();
 
 mailchimp.setConfig({
@@ -14,7 +14,7 @@ mailchimp.setConfig({
 
 // for the server to be able to use static files such as css and images, a folder where they are must be specified
 // solution was found in lection 223 comments
-// app.use(express.static("public")); Angela solution didn't work
+// app.use(express.static("public")); didn't work
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -38,49 +38,25 @@ app.post("/", (req, res) => {
     },
   ];
 
-  //   const jsonData = JSON.stringify(members);
-
-  //   const url = "https://us21.api.mailchimp.com/3.0/list/12cb840f5c";
-
-  //   const options = {
-  //     method: "POST",
-  //     auth: "pedro1:bd45645e9fe5ff5e8a91b4e9fc162860-us21",
-  //   };
-
-  //   const request = https.request(url, options, (response) => {
-  //     response.on("data", (dt) => {
-  //       console.log(JSON.parse(dt));
-  //     });
-  //   });
-
   // send request data to the mailchimp server
-  //   request.write(jsonData);
   async function run() {
     try {
       const response = await mailchimp.lists.batchListMembers("12cb840f5c", {
         members,
       });
-      //   const response = await mailchimp.lists.getList("12cb840f5c");
       if (response) {
         res.redirect("/success");
-        // res.sendFile(__dirname + "/success.html");
       }
     } catch (error) {
       const errData = JSON.stringify(error.response);
       console.log(errData);
       res.redirect("/failure");
-      //   res.sendFile(__dirname + "/failure.html");
     }
   }
 
   run();
-  //   request.end();
 });
 
 app.post("/failure", (req, res) => res.redirect("/"));
 
-app.listen(port, () =>
-  console.log(
-    `...........................................................App listening on port ${port}!`
-  )
-);
+app.listen(port || 3000, () => console.log(`App listening on port ${port}!`));
